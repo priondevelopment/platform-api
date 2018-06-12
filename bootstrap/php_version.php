90d454166db11a47db7fd18a64407b2012f40cee
Brandon Best <brandonbest@dreamideation.com>
2018-06-11 22:52:37 -0700

    Initial commit, includes a PHP version check/response

---
+++ bootstrap/php_version.php
@@ -0,0 +1,20 @@
+<?php
+
+checkPhpVersion();
+
+/**
+ * Check the PHP Version
+ */
+function checkPhpVersion ()
+{
+    $version = phpversion();
+    if ($version < 7) {
+        header('Content-type: application/json');
+        header("HTTP/1.1 500 Internal Server Error");
+
+        echo json_encode([
+            'error' => 1,
+            'message' => "Minimum PHP version is 7.0"]);
+        die;
+    }
+}
